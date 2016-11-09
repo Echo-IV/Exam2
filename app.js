9d3243d0f7749ea109928e4ca50b7193bb42eb0f
Alexis Phuong <alexis.hvp@gmail.com>
2016-11-09 15:34:16 +0100

    finalisation de l'exercice

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 (function(){
 
+var app = angular.module("app",[]);
+
+app.directive("header",function(){
+  return {
+    restrict:'A',
+    templateUrl:'partials/common/header.html'
+  }
+});
+
+app.directive("home",function(){
+  return {
+    restrict:'A',
+    templateUrl:'partials/home/home.html'
+  }
+});
+
+app.directive("footer",function(){
+  return {
+    restrict:'A',
+    templateUrl:'partials/common/footer.html'
+  }
+});
 
 
-
+app.controller("appController",function(){
+  this.personne = object;
+})
 
 
   var object = {
